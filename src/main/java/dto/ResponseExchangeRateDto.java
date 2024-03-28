8d125260841bac32cfa68edf5ebd6a59394f17ff
“ArturChegur” <a0030954@mail.ru>
2024-03-28 19:08:11 +0300

    white: deleted spaces and toString method in dto class

--- src/main/java/dto/ResponseExchangeRateDto.java
+++ src/main/java/dto/ResponseExchangeRateDto.java
@@ -44,14 +44,4 @@ public class ResponseExchangeRateDto {
     public int hashCode() {
         return Objects.hash(id);
     }
-
-    @Override
-    public String toString() {
-        return "{"
-               + "\"id\":" + id + ","
-               + "\"baseCurrency\":" + baseCurrency.toString() + ","
-               + "\"targetCurrency\":" + targetCurrency.toString() + ","
-               + "\"rate\":" + rate
-               + "}";
-    }
 }
--- src/main/java/util/ParameterValidator.java
+++ src/main/java/util/ParameterValidator.java
@@ -3,6 +3,7 @@ package util;
 public final class ParameterValidator {
     private ParameterValidator(){
     }
-    //todo methods for path validation and other
+
+    public static void
 
 }
